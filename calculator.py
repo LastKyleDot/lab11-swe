6ab68f7e6d9f54edd08bab06b922f0fb69f2bbf9
Kyle Hong <kylehong@Kyles-MacBook-Air.local>
2026-04-08 17:05:11 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,25 +1,24 @@
-# Kyle Hong partner 2
-import math
+import math # [cite: 81]
 
 def add(a, b):
-    return a + b
+    return a + b # [cite: 82]
 
 def sub(a, b):
-    return a - b
+    return a - b # [cite: 83, 85]
 
 def mul(a, b):
-    return a * b
+    return a * b # [cite: 86, 88]
 
 def div(a, b):
     if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
-    return b / a
+        raise ZeroDivisionError("Cannot divide by zero") # [cite: 91]
+    return b / a # [cite: 89, 91]
 
 def log(a, b):
-
+    # log base a of b [cite: 94]
     if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("Invalid input for logarithm")
-    return math.log(b, a)
+        raise ValueError("Invalid input for logarithm") # [cite: 94]
+    return math.log(b, a) # [cite: 94]
 
 def exp(a, b):
-    return a ** b
+    return a ** b # [cite: 95]
